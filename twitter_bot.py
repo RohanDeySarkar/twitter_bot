2ce3b90722ea4545e001b3543967147f06766d74
RohanDeySarkar <41055662+RohanDeySarkar@users.noreply.github.com>
2019-03-30 14:15:35 +0530

    Update twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,10 +1,12 @@
 import tweepy
 import time
+from textblob import TextBlob
+import random
 
-CONSUMER_KEY = '**************************'
-CONSUMER_SECRET = '***************************************'
-ACCESS_KEY = '*******************************************'
-ACCESS_SECRET = '***************************************'
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_KEY = ''
+ACCESS_SECRET = ''
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
@@ -12,6 +14,10 @@ api = tweepy.API(auth)
 
 FILE_NAME = 'last_seen_id.txt'
 
+##for follower in tweepy.Cursor(api.followers).items():
+##    follower.follow()
+##    print (follower.screen_name)
+     
 def retrieve_last_seen_id(file_name):
     f_read = open(file_name, 'r')
     last_seen_id = int(f_read.read().strip())
@@ -24,29 +30,43 @@ def store_last_seen_id(last_seen_id, file_name):  # this func modifies the last_
     f_write.close()
     return
 
+def tweet_sentiment(tweet):
+    
+    analysis = TextBlob(tweet)
+
+    if analysis.sentiment.polarity > 0:
+        return 'positive'
+    elif analysis.sentiment.polarity == 0:
+        return 'neutral'
+    else:
+        return 'negative'
+
 def reply_to_tweets():
+    
     print('retrieving and replying to tweets...', flush=True)
     
     last_seen_id = retrieve_last_seen_id(FILE_NAME)
-    # NOTE: We need to use tweet_mode='extended' below to show
-    # all full tweets (with full_text). Without it, long tweets
-    # would be cut off.
-    mentions = api.mentions_timeline(
-                        last_seen_id,
-                        tweet_mode='extended')
+
+    mentions = api.mentions_timeline(last_seen_id, tweet_mode='extended')                        
+                     
     for mention in reversed(mentions):
         print(str(mention.id) + ' - ' + mention.full_text, flush=True)
         last_seen_id = mention.id
         store_last_seen_id(last_seen_id, FILE_NAME)
-        if '#helloworld' in mention.full_text.lower():
-            print('found #helloworld!', flush=True)
-            print('responding back...', flush=True)
-            
-            api.update_status('@' + mention.user.screen_name +
-                    '#HelloWorld back to you!', mention.id)
 
+        sentiment = tweet_sentiment(mention.full_text.lower())
+
+        reply_neutral = ['Is that so', 'Maybe', 'Probably', 'Sure', 'Affirmative', 'thanks man']
+        reply_angry = ["I'm goona beat u up!!", "don't talk shit like that", "wanna see violence nigga"]
+        
+        if sentiment is 'positive':
+            api.update_status(random.choice(reply_neutral) + ',  @' + mention.user.screen_name, mention.id)
+        elif sentiment is 'neutral':
+            api.update_status(random.choice(reply_neutral) + ',  @' + mention.user.screen_name, mention.id)
+        else:
+            api.update_status(random.choice(reply_angry) + ',  @' + mention.user.screen_name, mention.id)
+                    
 while True:
     reply_to_tweets()
     time.sleep(15)
-        
 
